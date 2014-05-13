fa615d0090740087fbf4747bd4c00670caf10539
Gabriel Buades <gbuades@soffid.com>
2014-05-13 14:14:35 +0200

    IAM-588 Define workflow types

---
+++ meta/src/main/java/es/caib/bpm/vo/PredefinedProcessType.java
@@ -0,0 +1,8 @@
+package es.caib.bpm.vo;
+
+import com.soffid.mda.annotation.Enumeration;
+
+@Enumeration
+public class PredefinedProcessType {
+	public final String ROLE_APPROVAL = "RoleApproval";
+}

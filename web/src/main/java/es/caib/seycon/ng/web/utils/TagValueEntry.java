d6c292e0804c3fdb7ef80e5ce1122d7457a105cf
Gabriel Buades <gbuades@soffid.com>
2014-05-13 14:14:08 +0200

    IAM-585 Modify users screen

---
+++ web/src/main/java/es/caib/seycon/ng/web/utils/TagValueEntry.java
@@ -0,0 +1,18 @@
+package es.caib.seycon.ng.web.utils;
+
+public class TagValueEntry {
+	String tag;
+	String value;
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}

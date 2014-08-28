054946b7129396acdcd10d6273fa56fe9c8a8ed5
Gabriel Buades <gbuades@soffid.com>
2014-08-28 12:14:37 +0200

    Rename ROLES pacakge to roles

---
+++ meta/src/main/java/roles/user_create.java
@@ -0,0 +1,13 @@
+//
+// (C) 2013 Soffid
+// 
+// This file is licensed by Soffid under GPL v3 license
+//
+
+package roles;
+import com.soffid.mda.annotation.*;
+
+@Role (name="user:create" )
+public class user_create {
+	
+}

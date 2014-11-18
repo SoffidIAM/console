e08211eea52d025c1da78615a53c9879b70e9a4e
Gabriel Buades <gbuades@soffid.com>
2014-11-18 08:02:53 +0100

    IAM-653 Add account access levels

--- meta/src/main/java/es/caib/seycon/ng/model/AccountAccessEntity.java
+++ meta/src/main/java/es/caib/seycon/ng/model/AccountAccessEntity.java
@@ -7,7 +7,10 @@
 package es.caib.seycon.ng.model;
 import com.soffid.mda.annotation.*;
 
+import es.caib.seycon.ng.comu.AccountAccessLevelEnum;
+
 @Entity (table="SC_ACCACC" )
+@Description ("Contains the access control list for an account")
 @Depends ({es.caib.seycon.ng.model.AuditoriaEntity.class,
 	es.caib.seycon.ng.model.RolEntity.class,
 	es.caib.seycon.ng.model.GrupEntity.class,
@@ -15,24 +18,33 @@ import com.soffid.mda.annotation.*;
 	es.caib.seycon.ng.model.AccountEntity.class})
 public abstract class AccountAccessEntity {
 
+	@Description ("Grantee role")
 	@Column (name="AAC_ROL_ID")
 	@Nullable
 	public es.caib.seycon.ng.model.RolEntity rol;
 
+	@Description ("Grantee group")
 	@Column (name="AAC_GRU_ID")
 	@Nullable
 	public es.caib.seycon.ng.model.GrupEntity group;
 
+	@Description ("Grantee user")
 	@Column (name="AAC_USU_ID")
 	@Nullable
 	public es.caib.seycon.ng.model.UsuariEntity user;
 
+	@Description ("Granted accounte")
 	@Column (name="AAC_ACC_ID")
 	public es.caib.seycon.ng.model.AccountEntity account;
 
+	@Description ("Access level")
+	@Column (name="AAC_LEVEL", defaultValue=" es.caib.seycon.ng.comu.AccountAccessLevelEnum.ACCESS_OWNER")
+	public AccountAccessLevelEnum level;
+	
 	@Column (name="AAC_ID")
 	@Nullable
 	@Identifier
+	@Description("Identifier")
 	public java.lang.Long id;
 
 }

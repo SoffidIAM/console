68aa4f6c399041ad1920807eda279c1fea2de538
Gabriel Buades <gbuades@soffid.com>
2014-12-15 11:57:31 +0100

    Add PBKDF2 crypto routines

--- common/src/main/java/com/soffid/iam/crypt/Crypt.java
+++ common/src/main/java/com/soffid/iam/crypt/Crypt.java
@@ -1,12 +1,25 @@
 package com.soffid.iam.crypt;
 
 import java.io.UnsupportedEncodingException;
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.params.KeyParameter;
 
 import es.caib.seycon.util.Base64;
 
 public class Crypt
 {
+	static SecureRandom random = new SecureRandom();
+
 	public static String hash (String algorithm, String text)
 	{
 		if (algorithm == null || algorithm.trim().length() == 0)
@@ -15,6 +28,35 @@ public class Crypt
 		{
 			String salt = BCrypt.gensalt();
 			return BCrypt.hashpw(text, salt);
+		}
+		else if ("pBKDF2Sha256".equalsIgnoreCase(algorithm))
+		{
+			String salt;
+			try
+			{
+				salt = genSalt(8);
+				int iterations = 1200;
+				return ""+iterations+"$"+salt+"$"+pBKDF2Sha256(text, salt, iterations);
+			}
+			catch (UnsupportedEncodingException e)
+			{
+				throw new RuntimeException(e);
+			}
+		}
+		else if ("pBKDF2Sha1".equalsIgnoreCase(algorithm) ||
+				"pBKDF2".equalsIgnoreCase(algorithm))
+		{
+			String salt;
+			try
+			{
+				salt = genSalt(8);
+				int iterations = 1200;
+				return ""+iterations+"$"+salt+"$"+pBKDF2Sha1(text, salt, iterations);
+			}
+			catch (UnsupportedEncodingException e)
+			{
+				throw new RuntimeException(e);
+			}
 		}
 		else
 		{
@@ -30,6 +72,58 @@ public class Crypt
 				throw new RuntimeException(e);
 			}
 		}
+	}
+
+	public static String pBKDF2Sha256 (String text, String utf8Salt, int iterations ) throws UnsupportedEncodingException
+	{
+		return pBKDF2Sha256(text, Base64.decode(utf8Salt), iterations);
+    }
+	
+	public static String pBKDF2Sha256 (String text, byte []salt, int iterations ) throws UnsupportedEncodingException
+	{
+		PKCS5S2ParametersGenerator gen = new PKCS5S2ParametersGenerator(new SHA256Digest());
+		gen.init(text.getBytes("UTF-8"), salt, iterations);
+		byte[] dk = ((KeyParameter) gen.generateDerivedParameters(256)).getKey();
+		return Base64.encodeBytes(dk);
+    }
+
+	public static String pBKDF2Sha1(String text, String utf8Salt, int iterations ) throws UnsupportedEncodingException
+	{
+		return pBKDF2Sha256(text, Base64.decode(utf8Salt), iterations);
+    }
+	
+	public static String pBKDF2Sha1 (String text, byte []salt, int iterations ) throws UnsupportedEncodingException, InvalidKeySpecException, NoSuchAlgorithmException
+	{
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+	    KeySpec keyspec = new PBEKeySpec("password".toCharArray(), salt, 1000, 128);
+	    Key key = factory.generateSecret(keyspec);
+		byte[] dk = key.getEncoded();
+		return Base64.encodeBytes(dk);
+    }
+
+	public static byte [] genSaltBytes ( ) throws UnsupportedEncodingException
+	{
+		return genSaltBytes (8);
+	}
+
+	public static byte [] genSaltBytes ( int size ) throws UnsupportedEncodingException
+	{
+		StringBuffer rs = new StringBuffer();
+		byte rnd[] = new byte[size];
+
+		random.nextBytes(rnd);
+
+		return rnd;
+    }
+	
+	public static String genSalt () throws UnsupportedEncodingException
+	{
+		return Base64.encodeBytes(genSaltBytes());
+	}
+
+	public static String genSalt (int size) throws UnsupportedEncodingException
+	{
+		return Base64.encodeBytes(genSaltBytes(size));
 	}
 
 	public static boolean verify (String algorithm, String text, String hash)

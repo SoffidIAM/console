1a7fbd0f7d3716a7b0aec5d57bb8619b7e737a84
svives <svives@soffid.com>
2017-08-14 13:10:29 +0200

    New properties files

---
+++ webservice/src/main/java/com/soffid/iam/webservice/Messages.java
@@ -0,0 +1,20 @@
+package com.soffid.iam.webservice;
+
+import java.util.MissingResourceException;
+
+import com.soffid.iam.lang.MessageFactory;
+
+public class Messages
+{
+	private static final String BUNDLE_NAME = "com.soffid.iam.webservice.messages"; //$NON-NLS-1$
+
+	private Messages() {}
+
+	public static String getString(String key) {
+		try {
+			return MessageFactory.getString(BUNDLE_NAME,key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}

97f448dc3e74b29ab731ad6ab954ac96e188ac80
svives <svives@soffid.com>
2017-09-20 15:44:19 +0200

    Custom objects: new type enumerations

--- meta/src/main/java/es/caib/seycon/ng/comu/TypeEnumeration.java
+++ meta/src/main/java/es/caib/seycon/ng/comu/TypeEnumeration.java
@@ -1,27 +1,18 @@
-//
-// (C) 2013 Soffid
-// 
-// This file is licensed by Soffid under GPL v3 license
-//
-
 package es.caib.seycon.ng.comu;
-import com.soffid.mda.annotation.*;
 
-@Enumeration 
+import com.soffid.mda.annotation.Enumeration;
+
+@Enumeration
 public class TypeEnumeration {
 
-	public java.lang.String stringType="S";
-
-	public java.lang.String binaryType="B";
-
-	public java.lang.String photoType="P";
-
-	public java.lang.String dateType="D";
-
-	public java.lang.String emailType="E";
-
-	public java.lang.String userType="U";
-
-	public java.lang.String ssoFormType="F";
-
+	public String stringType = "S";
+	public String binaryType = "B";
+	public String photoType = "P";
+	public String dateType = "D";
+	public String emailType = "E";
+	public String userType = "U";
+	public String groupType = "G";
+	public String applicationType = "A";
+	public String customObjectType = "C";
+	public String ssoFormType = "F";
 }

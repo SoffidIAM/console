c3919a7e82540d81a8ba62f8f15530eafff12fef
Gabriel Buades <gbuades@soffid.com>
2018-04-08 12:41:18 +0200

    Italian language support

---
+++ web-common/src/main/java/com/soffid/iam/web/Languages.java
@@ -0,0 +1,7 @@
+package com.soffid.iam.web;
+
+public class Languages {
+	public static String[] langs = {"en", "es", "fr", "it", "nl", "ca"};
+	public static String[] descriptions = {"English", "Español", "Français", "Italiano", "Nederlandse", "Català"};
+
+}

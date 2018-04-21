8b23a06d45cb1e71c35972036a1c9f35274d63ac
Gabriel Buades <gbuades@soffid.com>
2018-04-21 20:55:25 +0200

    Correct workflow messages

--- core/src/main/java/com/soffid/iam/bpm/service/Messages.java
+++ core/src/main/java/com/soffid/iam/bpm/service/Messages.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public class Messages
 {
-	private static final String BUNDLE_NAME = "com.soffid.iam.bpm.servei.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.soffid.iam.bpm.service.messages"; //$NON-NLS-1$
 
 	private Messages()
 	{

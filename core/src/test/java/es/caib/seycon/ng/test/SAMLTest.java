16488854d9435401066bb71badce2ef6464d48ba
Gabriel Buades <gbuades@soffid.com>
2018-12-28 10:30:48 +0100

    Remove unneeded imports

--- core/src/test/java/es/caib/seycon/ng/test/SAMLTest.java
+++ core/src/test/java/es/caib/seycon/ng/test/SAMLTest.java
@@ -1,35 +1,12 @@
 package es.caib.seycon.ng.test;
 
-import java.math.BigInteger;
 import java.net.InetAddress;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.SignatureException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateEncodingException;
-import java.security.cert.X509Certificate;
-import java.util.Date;
-
-import org.bouncycastle.asn1.x509.X509Name;
-import org.bouncycastle.x509.X509V3CertificateGenerator;
 
 import com.soffid.iam.api.SamlRequest;
 import com.soffid.iam.service.SamlService;
 import com.soffid.iam.utils.ConfigurationCache;
 
 import es.caib.seycon.ng.ServiceLocator;
-import es.caib.seycon.ng.comu.Dispatcher;
-import es.caib.seycon.ng.comu.DominiContrasenya;
-import es.caib.seycon.ng.exception.InternalErrorException;
 import es.caib.seycon.ng.utils.Security;
 import es.caib.seycon.util.Base64;
 

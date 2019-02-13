09550b9322b13bb2cd582cf263a3c9253e1ec67a
Gabriel Buades <gbuades@soffid.com>
2019-02-13 16:44:45 +0100

    Needed for transactional actions

--- meta/src/main/java/com/soffid/iam/service/AsyncRunnerService.java
+++ meta/src/main/java/com/soffid/iam/service/AsyncRunnerService.java
@@ -3,11 +3,14 @@ package com.soffid.iam.service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.soffid.iam.api.AsyncList;
+import com.soffid.iam.common.TransactionalTask;
 import com.soffid.mda.annotation.Depends;
 import com.soffid.mda.annotation.Operation;
 import com.soffid.mda.annotation.Service;
 
-@Service(internal=true)
+import roles.Tothom;
+
+@Service
 @Depends({AsyncRunnerService.class})
 public class AsyncRunnerService {
 	@Operation()
@@ -17,4 +20,8 @@ public class AsyncRunnerService {
 	@Operation()
 	@Transactional(noRollbackFor = { java.lang.Exception.class }, readOnly=true)
 	public void runInternal (Runnable runnable, AsyncList result) {	}
+
+	@Operation(grantees={Tothom.class})
+	@Transactional(rollbackFor = { java.lang.Exception.class })
+	public Object runTransaction (TransactionalTask runnable) { return null; }
 }

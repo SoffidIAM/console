c74c7ea087ff6c889ce14ac062117883c4e45088
Gabriel Buades <gbuades@soffid.com>
2019-07-01 20:09:40 +0200

    Fixes for Oracle identity provider compatibility

--- core/src/main/java/com/soffid/iam/service/saml/SAMLServiceInternal.java
+++ core/src/main/java/com/soffid/iam/service/saml/SAMLServiceInternal.java
@@ -219,8 +219,9 @@ public class SAMLServiceInternal {
 		Response saml2Response = (Response) marshaller.unmarshall(doc.getDocumentElement());
 
 		log.info("Processing authentication response from "+saml2Response.getIssuer().getValue());
-		
-		if (! validateResponse(hostName, saml2Response))
+
+		boolean responseSigned = saml2Response.isSigned();
+		if (responseSigned && ! validateResponse(hostName, saml2Response))
 			return null;
 
 		String originalrequest = saml2Response.getInResponseTo();
@@ -239,14 +240,14 @@ public class SAMLServiceInternal {
 		for ( EncryptedAssertion encryptedAssertion: saml2Response.getEncryptedAssertions())
 		{
 			Assertion assertion = decrypt (encryptedAssertion);
-			if (validateAssertion(hostName, assertion))
+			if (validateAssertion(hostName, assertion, responseSigned))
 			{
 				return createAuthenticationRecord(hostName, requestEntity, assertion);
 			}
 		}
 		for ( Assertion assertion: saml2Response.getAssertions())
 		{
-			if (validateAssertion(hostName, assertion))
+			if (validateAssertion(hostName, assertion, responseSigned))
 			{
 				return createAuthenticationRecord(hostName, requestEntity, assertion);
 			}
@@ -778,7 +779,7 @@ public class SAMLServiceInternal {
     
     Log log = LogFactory.getLog(getClass());
     
-    private boolean validateAssertion (String hostName, Assertion assertion) throws ResolverException, InternalErrorException, ComponentInitializationException, AssertionValidationException, CertificateException, MalformedURLException
+    private boolean validateAssertion (String hostName, Assertion assertion, boolean responseSigned) throws ResolverException, InternalErrorException, ComponentInitializationException, AssertionValidationException, CertificateException, MalformedURLException
     {
     	SAML20AssertionValidator validator = getValidator(hostName);
     	
@@ -796,11 +797,14 @@ public class SAMLServiceInternal {
     	set.add(getBaseURL(hostName)+"saml/slo/redirect");
     	params.put (SAML2AssertionValidationParameters.SC_VALID_RECIPIENTS, set);
 
+    	if (responseSigned)
+    		params.put(SAML2AssertionValidationParameters.SIGNATURE_REQUIRED, Boolean.FALSE);
+
     	org.opensaml.saml.common.assertion.ValidationContext ctx = new ValidationContext(params);
 
-		ValidationResult result = validator.validate(assertion, ctx);
+    	ValidationResult result = validator.validate(assertion, ctx);
 		if (result != ValidationResult.VALID)
-			log.info("Error validating SAML message: "+ctx.getValidationFailureMessage());
+			log.info("Error validating Assertion: "+ctx.getValidationFailureMessage());
 		
 		if ( ! validDate (assertion.getIssueInstant()))
 			return false;

8e14f22081d539ac4e094d7941bab3d01decdfd8
Gabriel Buades <gbuades@soffid.com>
2019-10-08 10:29:21 +0200

    Translate logout page

--- weblogout/src/main/java/es/caib/seycon/ng/weblogout/LogoutServlet.java
+++ weblogout/src/main/java/es/caib/seycon/ng/weblogout/LogoutServlet.java
@@ -20,49 +20,49 @@ public class LogoutServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		String v_logoutTo = request.getParameter("logoutTo");
+		String v_logoutTo = request.getParameter("logoutTo"); //$NON-NLS-1$
 		if (v_logoutTo == null) {
-			v_logoutTo = "";
+			v_logoutTo = ""; //$NON-NLS-1$
 		}
 		
-		String lang = "ca";
+		String lang = "ca"; //$NON-NLS-1$
 		
 		//if (equest.getLocale()!=null) lang = equest.getLocale().getLanguage();
 		//if (lang==null) lang ="ca"; //per defecte
 
-		String msg_logout = "Your Soffid session has been closed.";
-		String msg_tornar = "Log in again";
+		String msg_logout = Messages.getString(request, "LogoutServlet.3"); //$NON-NLS-1$
+		String msg_tornar = Messages.getString(request, "LogoutServlet.4"); //$NON-NLS-1$
 
-		String html = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
-		html += "<html xmlns=\"http://www.w3.org/1999/xhtml\">"
-				+ "<head><meta http-equiv=\"Pragma\" content=\"no-cache\" />"
-				+ "<meta http-equiv=\"Expires\" content=\"-1\" />"
-				+ "<title>SOFFID IAM</title>"
-				+ "<style type=\"text/css\">"
-				+ "body {background-color: #EEE6DD;font-family:Arial,Helvetica,sans-serif;font-size:0.9em;margin:0px;font-size:small;} "
-				+ "table.messagebox {margin: 8px;}"
-				+ "div.ocult{ visibility:hidden; top:45%; width:100%;} "
-				+ "div.centered{ visibility: visible; position:absolute; top:45%; width:100%; text-align:center; ;z-index:324234;} "
-				+ ".g {color:gray; font-size: 12px;} "
-				+ "span {font-size:0.9em;} "
-				+ ".titol_tanca {color:#666666;cursor: pointer;font-family: Arial;font-size: 11px; font-weight: bold;text-decoration:none;} " 
-				+ ".titol_tanca:hover {text-decoration: underline;}"
-				+ ".cuadre {border: #bdd3ea 2px solid; background-color:white; padding:20px 40px; display:inline-block; width:400px;}"
-				+ ".byebye {padding: 5px 5px 20px 5px;}"
-				+ "</style>"
-				+ "</head>"
-				+ "<body>"
+		String html = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"; //$NON-NLS-1$
+		html += "<html xmlns=\"http://www.w3.org/1999/xhtml\">" //$NON-NLS-1$
+				+ "<head><meta http-equiv=\"Pragma\" content=\"no-cache\" />" //$NON-NLS-1$
+				+ "<meta http-equiv=\"Expires\" content=\"-1\" />" //$NON-NLS-1$
+				+ "<title>SOFFID IAM</title>" //$NON-NLS-1$
+				+ "<style type=\"text/css\">" //$NON-NLS-1$
+				+ "body {background-color: #EEE6DD;font-family:Arial,Helvetica,sans-serif;font-size:0.9em;margin:0px;font-size:small;} " //$NON-NLS-1$
+				+ "table.messagebox {margin: 8px;}" //$NON-NLS-1$
+				+ "div.ocult{ visibility:hidden; top:45%; width:100%;} " //$NON-NLS-1$
+				+ "div.centered{ visibility: visible; position:absolute; top:45%; width:100%; text-align:center; ;z-index:324234;} " //$NON-NLS-1$
+				+ ".g {color:gray; font-size: 12px;} " //$NON-NLS-1$
+				+ "span {font-size:0.9em;} " //$NON-NLS-1$
+				+ ".titol_tanca {color:#666666;cursor: pointer;font-family: Arial;font-size: 11px; font-weight: bold;text-decoration:none;} "  //$NON-NLS-1$
+				+ ".titol_tanca:hover {text-decoration: underline;}" //$NON-NLS-1$
+				+ ".cuadre {border: #bdd3ea 2px solid; background-color:white; padding:20px 40px; display:inline-block; width:400px;}" //$NON-NLS-1$
+				+ ".byebye {padding: 5px 5px 20px 5px;}" //$NON-NLS-1$
+				+ "</style>" //$NON-NLS-1$
+				+ "</head>" //$NON-NLS-1$
+				+ "<body>" //$NON-NLS-1$
 				// + "<div class=\"embedded\"><div class=\"wc-embedded-none\">"
-				+ "<div class=\"centered\"><div class=\"cuadre\"><div class=\"byebye\">"
-				+ "<span>"+msg_logout+"</span>"
-				+ "</div><span class=\"g\">&lt;&lt; </span>"
-				+ "<a href=\""
-				+ "../" + v_logoutTo
-				+ "\" class=\"titol_tanca\">"+msg_tornar+"</a></div></div></div>"
+				+ "<div class=\"centered\"><div class=\"cuadre\"><div class=\"byebye\">" //$NON-NLS-1$
+				+ "<span>"+msg_logout+"</span>" //$NON-NLS-1$ //$NON-NLS-2$
+				+ "</div><span class=\"g\">&lt;&lt; </span>" //$NON-NLS-1$
+				+ "<a href=\"" //$NON-NLS-1$
+				+ "../" + v_logoutTo //$NON-NLS-1$
+				+ "\" class=\"titol_tanca\">"+msg_tornar+"</a></div></div></div>" //$NON-NLS-1$ //$NON-NLS-2$
 				// + "</div></div>"
-				+ "</body></html>";
+				+ "</body></html>"; //$NON-NLS-1$
 
-		response.setContentType("text/html; charset=UTF-8");
+		response.setContentType("text/html; charset=UTF-8"); //$NON-NLS-1$
 
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println(html);
---
+++ weblogout/src/main/java/es/caib/seycon/ng/weblogout/Messages.java
@@ -0,0 +1,60 @@
+package es.caib.seycon.ng.weblogout;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "es.caib.seycon.ng.weblogout.messages"; //$NON-NLS-1$
+
+	static Map<Locale, ResourceBundle> map =  new HashMap<Locale, ResourceBundle>();
+
+	private Messages() {
+	}
+
+
+	static Map<String, Map<Locale, ResourceBundle>> bundles = new HashMap<String, Map<Locale,ResourceBundle>>();
+
+    public static String getString(Locale l, String bundleName, String message) {
+    	ResourceBundle resources;
+    	synchronized (bundles) {
+    		resources = map.get(l);
+    		if (resources == null) {
+    			try {
+        			resources = ResourceBundle.getBundle(BUNDLE_NAME, l,
+            				ResourceBundle.Control.getNoFallbackControl(
+            					ResourceBundle.Control.FORMAT_PROPERTIES));
+
+    			} catch (MissingResourceException e)
+    			{
+        			resources = ResourceBundle.getBundle(BUNDLE_NAME, l,
+        					Thread.currentThread().getContextClassLoader(),
+            				ResourceBundle.Control.getNoFallbackControl(
+            					ResourceBundle.Control.FORMAT_PROPERTIES));
+
+    			}
+    			map.put(l, resources);
+    		}
+    	}
+    	return resources.getString(message);
+    }
+
+    public static String getString(HttpServletRequest request, String key) {
+		try {
+			String lang = request.getHeader("Accept-Language");
+			if (lang == null)
+				return getString(Locale.getDefault(), BUNDLE_NAME, key);
+			else
+			{
+				lang = lang.trim().split("[,_-]+")[0];
+				return getString(new Locale(lang), BUNDLE_NAME, key);
+			}
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}

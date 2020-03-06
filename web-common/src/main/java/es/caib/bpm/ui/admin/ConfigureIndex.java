7fc8f8185fe1aeed2c6366e4fae601d249a71485
Gabriel Buades <gbuades@soffid.com>
2020-03-06 16:29:45 +0100

    Search in global JNDI

--- web-common/src/main/java/es/caib/bpm/ui/admin/ConfigureIndex.java
+++ web-common/src/main/java/es/caib/bpm/ui/admin/ConfigureIndex.java
@@ -105,7 +105,7 @@ public class ConfigureIndex extends Frame
 
 	public JbpmSchedulerServiceInterface getJobSchedulerEjb() throws NamingException {
 		return (JbpmSchedulerServiceInterface) new InitialContext()
-				.lookup("openejb:/local/JbpmSchedulerServiceLocal");
+				.lookup("/JbpmSchedulerServiceLocal");
 	}
 
 	public void configure () throws IOException, CreateException,

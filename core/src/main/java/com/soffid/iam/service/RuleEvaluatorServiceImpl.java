bc9ab9da2e86119e3578cbd7bb970a54d8cab6c1
Gabriel Buades <gbuades@soffid.com>
2020-05-07 11:33:24 +0200

    Do not revoke expired grants

--- core/src/main/java/com/soffid/iam/service/RuleEvaluatorServiceImpl.java
+++ core/src/main/java/com/soffid/iam/service/RuleEvaluatorServiceImpl.java
@@ -49,6 +49,7 @@ import es.caib.seycon.ng.exception.NeedsAccountNameException;
 
 import java.io.File;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -127,7 +128,12 @@ public class RuleEvaluatorServiceImpl extends RuleEvaluatorServiceBase implement
 			// Now remove unneded roles
 			for (RoleAccountEntity role : roles) {
                 if (role.getRule() != null && role.getRule().getId().equals(rule.getId())) {
-                    method.revoke(user, role);
+                	if (role.isEnabled() || 
+                			role.getEndDate() == null ||
+                			role.getEndDate().after(new Date()))
+                	{
+                		method.revoke(user, role);
+                	}
                 }
             }
 		} catch (Exception e) {

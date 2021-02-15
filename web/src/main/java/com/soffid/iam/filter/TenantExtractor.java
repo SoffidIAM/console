dda1767fe32e72c1ab3af4dcf388b11a1688871d
Gabriel Buades <gbuades@soffid.com>
2021-02-15 10:53:29 +0100

    Fix tenant extractor

--- web/src/main/java/com/soffid/iam/filter/TenantExtractor.java
+++ web/src/main/java/com/soffid/iam/filter/TenantExtractor.java
@@ -3,15 +3,20 @@ package com.soffid.iam.filter;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.soffid.iam.api.Tenant;
 import com.soffid.iam.utils.ConfigurationCache;
 import com.soffid.iam.utils.Security;
 
+import es.caib.seycon.ng.ServiceLocator;
 import es.caib.seycon.ng.exception.InternalErrorException;
 
 public class TenantExtractor {
+	static Hashtable<String,Long> cache = new Hashtable<>();
 	public String getTenant (HttpServletRequest req) throws InternalErrorException
 	{
 		String hostName = req.getServerName();
@@ -33,6 +38,16 @@ public class TenantExtractor {
 			tenantHost = hostParts[0];
 			if (tenantHost.isEmpty())
 				tenantHost = Security.getMasterTenantName();
+			else {
+				Long l = cache.get(tenantHost);
+				if ( l == null || l.longValue() < System.currentTimeMillis() - 20000) {
+		    		Tenant tenant = ServiceLocator.instance().getTenantService().getTenant(tenantHost);
+		    		if (tenant == null || !tenant.isEnabled())
+		    			tenantHost = Security.getMasterTenantName();
+		    		else
+		    			cache.put(tenantHost, new Long(System.currentTimeMillis()));
+				}
+			}
 		} else
 			tenantHost = Security.getMasterTenantName();
 		

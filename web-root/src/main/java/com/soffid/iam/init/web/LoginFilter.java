55f04c55092e094e410af19a86015c2943f6cc15
Gabriel Buades <gbuades@soffid.com>
2021-05-05 15:48:21 +0200

    Redirect for webservice URL compatibility

--- web-root/src/main/java/com/soffid/iam/init/web/LoginFilter.java
+++ web-root/src/main/java/com/soffid/iam/init/web/LoginFilter.java
@@ -31,6 +31,9 @@ public class LoginFilter implements Filter {
 				if ( req.getRequestURI().startsWith("/selfservice/task.zul")) {
 					resp.sendRedirect("/soffid/wf/task.zul?"+req.getQueryString());
 				}
+				else if ( req.getRequestURI().startsWith("/webservice")) {
+					resp.sendRedirect("/soffid"+req.getRequestURI()+"?"+req.getQueryString());
+				}
 				else if ( req.getRequestURI().startsWith("/selfservice/index.zul") &&
 						req.getParameter("target") != null &&
 						req.getParameter("target").startsWith("sharedAccounts/sharedAccounts.zul")) {

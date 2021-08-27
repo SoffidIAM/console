fcf69de175dd89e64c4bddf4f174eb0e43d764fc
Gabriel Buades <gbuades@soffid.com>
2021-08-27 11:39:10 +0200

    Reorder account status

--- meta/src/main/java/com/soffid/iam/api/AccountStatus.java
+++ meta/src/main/java/com/soffid/iam/api/AccountStatus.java
@@ -5,9 +5,9 @@ import com.soffid.mda.annotation.Enumeration;
 @Enumeration
 public class AccountStatus {
 	public static String ACTIVE = "a";
-	public static String DISABLED = "d";
 	public static String FORCED_ACTIVE = "FA";
+	public static String LOCKED = "l";
+	public static String DISABLED = "d";
 	public static String FORCED_DISABLED = "FD";
 	public static String REMOVED = "r";
-	public static String LOCKED = "l";
 }

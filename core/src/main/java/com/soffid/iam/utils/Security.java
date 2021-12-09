64e178872c3d975d691524eb1a6cdff5ed2095d6
Gabriel Buades <gbuades@soffid.com>
2021-12-09 12:02:42 +0100

    Improved trusted ip filters

--- core/src/main/java/com/soffid/iam/utils/Security.java
+++ core/src/main/java/com/soffid/iam/utils/Security.java
@@ -3,6 +3,8 @@ package com.soffid.iam.utils;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.security.AccessController;
 import java.security.Principal;
@@ -740,7 +742,40 @@ public class Security {
 		return clientIp.get();
 	}
 	
-	public static void setClientRequest (HttpServletRequest req)
+	static Collection<NetmaskMatch> matches = null;
+	protected static void parseTrustedIps(String s) throws UnknownHostException {
+		for (String part: s.split("\\s*,\\s*")) {
+			if (part.contains("*")) {
+				part = part.substring(0, part.indexOf("*"));
+				int dots = 0;
+				int pos = 0;
+				do {
+					int i = part.indexOf(".", pos);
+					if (i < 0) break;
+					dots ++;
+					pos = i+1;
+				} while(true);
+				if (dots == 0)
+					part = part + "0.0.0.0/0";
+				else if (dots == 1)
+					part = part + "0.0.0/8";
+				else if (dots == 2)
+					part = part + "0.0/16";
+				else
+					part = part + "0/24";
+			}
+			int slash = part.indexOf("/");
+			if (slash < 0) {
+				byte[] addr = InetAddress.getByName(part).getAddress();
+				matches.add(new NetmaskMatch(addr, addr.length * 8));
+			} else {
+				byte[] addr = InetAddress.getByName(part.substring(0, slash)).getAddress();
+				int bits = Integer.parseInt(part.substring(slash+1));
+				matches.add(new NetmaskMatch(addr, bits));
+			}
+		}
+	}
+	public static void setClientRequest (HttpServletRequest req) throws UnknownHostException
 	{
 		String ip = req.getRemoteAddr();
 		String trustedProxies;
@@ -755,8 +790,7 @@ public class Security {
 		String forwardedFor = req.getHeader("x-forwarded-for");
 		if (trustedProxies != null)
 		{
-			String[] tp = trustedProxies.split("[, ]+");
-			if ( isTrusted (ip, tp) )
+			if ( isTrusted (ip, trustedProxies) )
 			{
 				if (forwardedFor == null || forwardedFor.trim().isEmpty())
 				{
@@ -770,7 +804,7 @@ public class Security {
 					for (int i = ff.length - 1; i >= 0; i--)
 					{
 						ip = ff[i];
-						if ( ! isTrusted(ip, tp))
+						if ( ! isTrusted(ip, trustedProxies))
 							break;
 					}
 				}
@@ -779,19 +813,44 @@ public class Security {
 		setClientIp(ip);
 	}
 
-	private static boolean isTrusted(String ip, String[] tp) {
-		for (String t: tp)
-			if (ip.equalsIgnoreCase(t))
+	private static boolean isTrusted(String ip, String trustedProxies) throws UnknownHostException {
+		if (matches == null) parseTrustedIps(trustedProxies);
+		byte[] addrBytes = InetAddress.getByName(ip).getAddress();
+		for (NetmaskMatch m: matches) {
+			if (m.match(addrBytes))
 				return true;
-			else if (t.contains("*"))
-			{
-				String t2 = t.replaceAll("\\*", "[0-9]*");
-				if ( Pattern.matches("^"+t2+"$", ip))
-					return true;
-			}
+		}
 		return false;
 	}
 	
+	static class NetmaskMatch {
+		public NetmaskMatch(byte[] address, int bits) {
+			super();
+			this.address = address;
+			this.bits = bits;
+		}
+		static byte pattern[] = new byte [] { 0, (byte) 0x80, (byte) 0xc0, (byte) 0xe0, (byte) 0xf0, (byte) 0xf8, (byte) 0xfc, (byte) 0xfe, (byte) 0xff} ;
+		byte address[];
+		int bits;
+		public boolean match (byte[] addr2) {
+			if (addr2.length  != address.length)
+				return false;
+			for (int pos = 0, i = 0; pos < addr2.length && i < bits; i += 8, pos ++) {
+				if ( bits - i >= 8 ) {
+					if (addr2[pos] != address[pos])
+						return false;
+				}
+				else
+				{
+					byte p = pattern [bits-i];
+					if ( (addr2[pos] & p) != (address[pos] & p))
+						return false;
+				}
+			}
+			return true;
+		}
+	}
+
 	static boolean debugNestedLogin() {
 		return "true".equals(System.getProperty("soffid.nestedLogin.debug"));
 	}

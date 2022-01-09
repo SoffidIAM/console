4da95bedad0a549dfc4c9849c8c8cf0d71e90e63
Gabriel Buades <gbuades@soffid.com>
2022-01-09 17:47:42 +0100

    Fix date converter in Java 11

--- common/src/main/java/com/soffid/iam/json/CalendarConverter.java
+++ common/src/main/java/com/soffid/iam/json/CalendarConverter.java
@@ -3,21 +3,32 @@ package com.soffid.iam.json;
 import org.apache.johnzon.mapper.Converter;
 
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
-import javax.xml.bind.DatatypeConverter;
 
 public class CalendarConverter implements Converter<Calendar>,
 	Converter.TypeAccess
 {
+	static SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 	@Override
     public String toString(Calendar instance) {
-        return DatatypeConverter.printDateTime(instance);
+		if (instance == null)
+			return "";
+        return dt.format(instance.getTime());
     }
  
 	@Override
     public Calendar fromString(final String text) {
-        return DatatypeConverter.parseDateTime(text);
+		if (text == null || text.trim().isEmpty())
+			return null;
+		Calendar c = Calendar.getInstance();
+		try {
+			c.setTime(dt.parse(text));
+		} catch (ParseException e) {
+			throw new RuntimeException("Unable to parse date "+text);
+		}
+		return c;
     }
 
 	@Override
--- common/src/main/java/com/soffid/iam/json/DateConverter.java
+++ common/src/main/java/com/soffid/iam/json/DateConverter.java
@@ -1,25 +1,32 @@
 package com.soffid.iam.json;
 
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-
-import javax.xml.bind.DatatypeConverter;
 
 import org.apache.johnzon.mapper.Converter;
 
 public class DateConverter implements Converter<Date>, Converter.TypeAccess
 {
+	static SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 	@Override
 	public String toString(Date date) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(date);
-		return DatatypeConverter.printDateTime(calendar);
+		if (date == null)
+			return "";
+		return dt.format(date);
 	}
 
 	@Override
 	public Date fromString(String text) {
-		return DatatypeConverter.parseDateTime(text).getTime();
+		if (text == null || text.trim().isEmpty())
+			return null;
+		try {
+			return dt.parse(text);
+		} catch (ParseException e) {
+			throw new RuntimeException("Unable to parse date "+text);
+		}
 	}
 
 	@Override

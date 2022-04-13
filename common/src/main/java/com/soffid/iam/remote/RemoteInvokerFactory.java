832e143bd9847e2ee450befa0072e45089f62eb2
Gabriel Buades <gbuades@soffid.com>
2022-04-13 11:01:46 +0200

    Raise java privileges

--- common/src/main/java/com/soffid/iam/remote/RemoteInvokerFactory.java
+++ common/src/main/java/com/soffid/iam/remote/RemoteInvokerFactory.java
@@ -8,6 +8,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.rmi.RemoteException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
@@ -30,60 +34,64 @@ public class RemoteInvokerFactory {
     }
     
     public Object getInvoker(URL url, String tenantName, String authToken) throws IOException {
-        HttpURLConnection c;
         // Cambiar la factoria SSL
         try {
+        	return AccessController.doPrivileged(new PrivilegedExceptionAction<Object>() {
+				@Override
+				public Object run() throws Exception {
+					HttpURLConnection c;
+		            c = ConnectionFactory.getConnection(url);
 
-            c = ConnectionFactory.getConnection(url);
-
-            c.setDoInput(true);
-            c.setDoOutput(false);
-            c.setAllowUserInteraction(false);
-            if (authToken != null) {
-                String seu = "-seu-"+tenantName+":"+authToken; //$NON-NLS-1$
-                byte bytes[] = seu.getBytes("UTF-8"); //$NON-NLS-1$
-                String tag = "Basic "+ Base64.encodeBytes(bytes, 0, bytes.length, Base64.DONT_BREAK_LINES); //$NON-NLS-1$
-                c.addRequestProperty("Authorization", tag); //$NON-NLS-1$
-                
-            }
-            Locale locale = MessageFactory.getLocale();
-            if (locale != null)
-            	c.addRequestProperty("Accept-Language", locale.toString());
-            try {
-            	c.connect();
-            } catch (IOException e) {
-            	try {
-            		ConnectionFactory.reloadKeys();
-            	} catch (Exception e2) {}
-            	throw e;
-            }
-            
-            // Consumir el stream
-            InputStream in = c.getInputStream();
-            while (in.read() >= 0);
-            
-            String classes = c.getHeaderField("Classes"); //$NON-NLS-1$
-            if (classes == null)
-                throw new IOException("Invalid response from " + url); //$NON-NLS-1$
-            String[] split = classes.split("[, ]+"); //$NON-NLS-1$
-            Class[] interfaces = new Class[split.length];
-            for (int i = 0; i < split.length; i++) {
-            	try {
-	                interfaces[i] = Class.forName(split[i], false, 
-	                		RemoteInvokerFactory.class.getClassLoader());
-            	} catch (ClassNotFoundException e) {
-	                interfaces[i] = Class.forName(split[i], false, 
-	                		Thread.currentThread().getContextClassLoader());
-            	}
-            }
-            HttpInvokerHandler invoker = new HttpInvokerHandler(url, tenantName, authToken);
-            invoker.setHeadersFactory(headersFactory);
-            
-			return Proxy.newProxyInstance(RemoteInvokerFactory.class.getClassLoader(),
-                    interfaces, invoker);
+		            c.setDoInput(true);
+		            c.setDoOutput(false);
+		            c.setAllowUserInteraction(false);
+		            if (authToken != null) {
+		                String seu = "-seu-"+tenantName+":"+authToken; //$NON-NLS-1$
+		                byte bytes[] = seu.getBytes("UTF-8"); //$NON-NLS-1$
+		                String tag = "Basic "+ Base64.encodeBytes(bytes, 0, bytes.length, Base64.DONT_BREAK_LINES); //$NON-NLS-1$
+		                c.addRequestProperty("Authorization", tag); //$NON-NLS-1$
+		                
+		            }
+		            Locale locale = MessageFactory.getLocale();
+		            if (locale != null)
+		            	c.addRequestProperty("Accept-Language", locale.toString());
+		            try {
+		            	c.connect();
+		            } catch (IOException e) {
+		            	try {
+		            		ConnectionFactory.reloadKeys();
+		            	} catch (Exception e2) {}
+		            	throw e;
+		            }
+		            
+		            // Consumir el stream
+		            InputStream in = c.getInputStream();
+		            while (in.read() >= 0);
+		            
+		            String classes = c.getHeaderField("Classes"); //$NON-NLS-1$
+		            if (classes == null)
+		                throw new IOException("Invalid response from " + url); //$NON-NLS-1$
+		            String[] split = classes.split("[, ]+"); //$NON-NLS-1$
+		            Class[] interfaces = new Class[split.length];
+		            for (int i = 0; i < split.length; i++) {
+		            	try {
+			                interfaces[i] = Class.forName(split[i], false, 
+			                		RemoteInvokerFactory.class.getClassLoader());
+		            	} catch (ClassNotFoundException e) {
+			                interfaces[i] = Class.forName(split[i], false, 
+			                		Thread.currentThread().getContextClassLoader());
+		            	}
+		            }
+		            HttpInvokerHandler invoker = new HttpInvokerHandler(url, tenantName, authToken);
+		            invoker.setHeadersFactory(headersFactory);
+		            
+					return Proxy.newProxyInstance(RemoteInvokerFactory.class.getClassLoader(),
+		                    interfaces, invoker);
+				}
+			});
+        } catch (PrivilegedActionException e) {
+            throw new RemoteException ("Nested exception", e.getCause()); //$NON-NLS-1$
         } catch (IllegalArgumentException e) {
-            throw new RemoteException ("Nested exception", e); //$NON-NLS-1$
-        } catch (ClassNotFoundException e) {
             throw new RemoteException ("Nested exception", e); //$NON-NLS-1$
         }
     }
--- common/src/main/java/com/soffid/iam/ssl/HttpInvokerHandler.java
+++ common/src/main/java/com/soffid/iam/ssl/HttpInvokerHandler.java
@@ -10,6 +10,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.rmi.RemoteException;
+import java.security.AccessController;
+import java.security.PrivilegedExceptionAction;
 import java.util.List;
 import java.util.Locale;
 
@@ -39,64 +41,73 @@ public class HttpInvokerHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args)
             throws Throwable {
 
-        Object result = null;
-        HttpURLConnection c;
+    	Object result = null;
+    	String[] success = new String[1];
         try {
-            
-            // Cambiar la factoria SSL
-            c = ConnectionFactory.getConnection(url);
-            
-            c.setDoInput(true);
-            c.setDoOutput(true);
-            c.setRequestMethod("POST"); //$NON-NLS-1$
-            if (authToken != null) {
-                String seu = "-seu-";  //$NON-NLS-1$
-                if (tenantName != null)
-                	seu = seu + URLEncoder.encode(tenantName, "UTF-8");
-                seu = seu + ":" + authToken;
-                byte bytes[] = seu.getBytes("UTF-8"); //$NON-NLS-1$
-                String tag = "Basic "+ Base64.encodeBytes(bytes, 0, bytes.length, Base64.DONT_BREAK_LINES);  //$NON-NLS-1$
-                c.addRequestProperty("Authorization", tag); //$NON-NLS-1$
-                
-            }
-            if (headersFactory != null)
-            {
-            	for ( HeadersFactory h: headersFactory)
-            		h.addHeaders(c);
-            }
-            
-            Locale locale = MessageFactory.getLocale();
-            c.addRequestProperty (HttpHeaders.ACCEPT_LANGUAGE, locale.toString() );
-            
-            c.connect();
-            ObjectOutputStream oout = new ObjectOutputStream (c.getOutputStream());
+        	PrivilegedExceptionAction<Object> action = new PrivilegedExceptionAction<Object>() {
+				@Override
+				public Object run() throws Exception {
+		        	HttpURLConnection c;
+		            // Cambiar la factoria SSL
+		            c = ConnectionFactory.getConnection(url);
+		            
+		            c.setDoInput(true);
+		            c.setDoOutput(true);
+		            c.setRequestMethod("POST"); //$NON-NLS-1$
+		            if (authToken != null) {
+		                String seu = "-seu-";  //$NON-NLS-1$
+		                if (tenantName != null)
+		                	seu = seu + URLEncoder.encode(tenantName, "UTF-8");
+		                seu = seu + ":" + authToken;
+		                byte bytes[] = seu.getBytes("UTF-8"); //$NON-NLS-1$
+		                String tag = "Basic "+ Base64.encodeBytes(bytes, 0, bytes.length, Base64.DONT_BREAK_LINES);  //$NON-NLS-1$
+		                c.addRequestProperty("Authorization", tag); //$NON-NLS-1$
+		                
+		            }
+		            if (headersFactory != null)
+		            {
+		            	for ( HeadersFactory h: headersFactory)
+		            		h.addHeaders(c);
+		            }
+		            
+		            Locale locale = MessageFactory.getLocale();
+		            c.addRequestProperty (HttpHeaders.ACCEPT_LANGUAGE, locale.toString() );
+		            
+		            c.connect();
+		            ObjectOutputStream oout = new ObjectOutputStream (c.getOutputStream());
 
-            oout.writeUTF(method.getName());
-            
-            int len  = args == null? 0 : args.length;
-            oout.writeInt(len);
-            for ( int i = 0; i < len; i++)
-            {
-                Class clazz = method.getParameterTypes()[i];
-                oout.writeUTF(clazz.getName());
-            }
-            
-            for ( int i = 0; i < len; i++)
-            {
-                oout.writeObject (args[i]);
-            }
+		            oout.writeUTF(method.getName());
+		            
+		            int len  = args == null? 0 : args.length;
+		            oout.writeInt(len);
+		            for ( int i = 0; i < len; i++)
+		            {
+		                Class clazz = method.getParameterTypes()[i];
+		                oout.writeUTF(clazz.getName());
+		            }
+		            
+		            for ( int i = 0; i < len; i++)
+		            {
+		                oout.writeObject (args[i]);
+		            }
 
-            oout.close();
-            
-            ObjectInputStream oin = new ObjectInputStream (c.getInputStream());
-            result = oin.readObject();
-            oin.close ();
-            
+		            oout.close();
+		            
+		            ObjectInputStream oin = new ObjectInputStream (c.getInputStream());
+		            Object result = oin.readObject();
+		            oin.close ();
+		            
+		            success[0] = c.getHeaderField("Success");
+		            
+		            return result;
+				}
+			};
+        	result = AccessController.doPrivileged(action);
         } catch (Exception e) {
             throw new RemoteException (Messages.getString("HttpInvokerHandler.5"), e); //$NON-NLS-1$
         }
 
-        if ("true".equals(c.getHeaderField("Success"))) //$NON-NLS-1$ //$NON-NLS-2$
+        if ("true".equals(success[0])) //$NON-NLS-1$ //$NON-NLS-2$
         {
             return result;
         }

0f17f1e14ae39c056dc0e09596fa30e6fa028136
Gabriel Buades <gbuades@soffid.com>
2022-08-22 16:48:27 +0200

    Asynchronous method to apply rules

---
+++ meta/src/main/java/com/soffid/iam/api/ApplyRuleProcess.java
@@ -0,0 +1,27 @@
+package com.soffid.iam.api;
+
+import java.util.Date;
+
+import com.soffid.mda.annotation.Nullable;
+import com.soffid.mda.annotation.ValueObject;
+
+@ValueObject
+public class ApplyRuleProcess {
+	Long id;
+	
+	float progress;
+	
+	@Nullable String report;
+	
+	boolean finished;
+	
+	boolean cancelled;
+	
+	@Nullable String errorMessage;
+	
+	@Nullable Date start;
+	
+	@Nullable Date end;
+	
+	@Nullable String current;
+}

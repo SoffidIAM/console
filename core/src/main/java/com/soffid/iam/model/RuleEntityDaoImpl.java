6b066b8bfe42a5153818b641fa5813d15cbc3887
Gabriel Buades <gbuades@soffid.com>
2023-05-09 16:13:36 +0200

    Disconnect disabled grants from rules before removing a rule

--- core/src/main/java/com/soffid/iam/model/RuleEntityDaoImpl.java
+++ core/src/main/java/com/soffid/iam/model/RuleEntityDaoImpl.java
@@ -12,4 +12,14 @@ import com.soffid.iam.api.Rule;
  */
 public class RuleEntityDaoImpl extends RuleEntityDaoBase
 {
+	@Override
+	public void remove(RuleEntity entity) {
+		getSession().createQuery("update com.soffid.iam.model.RoleAccountEntity "
+				+ "set rule = null "
+				+ "where enabled = :false and rule.id=:id")
+			.setBoolean("false", false)
+			.setLong("id", entity.getId())
+			.executeUpdate();
+		super.remove(entity);
+	}
 }

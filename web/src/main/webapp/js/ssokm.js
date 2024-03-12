485e6936bdcd73e7c4dbef2be431f5e8cb444463
Gabriel Buades <gbuades@soffid.com>
2024-03-12 09:20:03 +0100

    Add edge and safari extensions

--- web/src/main/webapp/js/ssokm.js
+++ web/src/main/webapp/js/ssokm.js
@@ -39,9 +39,11 @@ function installSSOExtension() {
 			var winNav = window.navigator;
 			var vendorName = winNav.vendor;
 			var isOpera = typeof window.opr !== "undefined";
-			var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+			var isEdge = winNav.userAgent.match("Edg/");
 			var isIOSChrome = winNav.userAgent.match("CriOS");
 			var isFirefox = winNav.userAgent.match("Firefox/");
+			var isChrome = winNav.userAgent.match("Chrome/");
+			var isSafari = winNav.userAgent.match("Safari/");
 			
 			if ( isFirefox) {
 				if (confirm ("A Firefox extension must be installed to use the Password Manager. Please, confirm to install it. After installing the addon, you can come backup to Soffid self service portal"))
@@ -52,9 +54,22 @@ function installSSOExtension() {
 			} else if ( isEdge) {
 				if (confirm ("An Edge extension must be installed to use the Password Manager. Please, confirm to install it. After installing the addon, you can come backup to Soffid self service portal"))
 				{
-					location.href="https://microsoftedge.microsoft.com/addons/detail/bnkdlnnmeeaifncdjedofbdpmipicljc";
+					location.href="https://microsoftedge.microsoft.com/addons/detail/soffid-password-manager/bnkdlnnmeeaifncdjedofbdpmipicljc";
 					return;
 				}
+			} else if (isChrome) {
+				if (confirm ("A Google chrome extension must be installed to user the Password Manager. Please, confirm to install it. After installing the addon, you can come backup to Soffid self service portal"))
+				{
+					location.href="https://chrome.google.com/webstore/detail/phjdhfhnbedpkmplaegoejildnieofcf";
+					return;
+				}
+			} else if ( isSafari) {
+				if (confirm ("A Safari extension must be installed to use the Password Manager. Please, confirm to install it. After installing the addon, you can come backup to Soffid self service portal"))
+				{
+					location.href="https://apps.apple.com/us/app/soffid-password-manager/id6476981158";
+					return;
+				}
+				
 			} else {
 				if (confirm ("A Google chrome extension must be installed to user the Password Manager. Please, confirm to install it. After installing the addon, you can come backup to Soffid self service portal"))
 				{

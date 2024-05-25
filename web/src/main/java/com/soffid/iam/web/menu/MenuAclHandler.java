542f8053f41a83e9ef5d009359e80172cb9a2f98
Sion Vives Bernat <svives@soffid.com>
2024-05-25 13:16:09 +0200

    Application access tree - Error in authorisations options #90

--- web/src/main/java/com/soffid/iam/web/menu/MenuAclHandler.java
+++ web/src/main/java/com/soffid/iam/web/menu/MenuAclHandler.java
@@ -1,27 +1,49 @@
 package com.soffid.iam.web.menu;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.CreateException;
 import javax.naming.NamingException;
 
 import org.zkoss.util.resource.Labels;
+import org.zkoss.xel.fn.CommonFns;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.HtmlBasedComponent;
 import org.zkoss.zk.ui.Path;
+import org.zkoss.zk.ui.UiException;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.ext.AfterCompose;
 import org.zkoss.zul.Div;
 import org.zkoss.zul.Window;
 
 import com.soffid.iam.EJBLocator;
+import com.soffid.iam.api.AccessTree;
 import com.soffid.iam.api.AccessTreeAuthorization;
+import com.soffid.iam.api.Account;
 import com.soffid.iam.api.Role;
+import com.soffid.iam.api.User;
+import com.soffid.iam.service.ejb.ApplicationService;
+import com.soffid.iam.service.ejb.EntryPointService;
+import com.soffid.iam.service.ejb.GroupService;
 import com.soffid.iam.service.ejb.UserService;
+import com.soffid.iam.service.ejb.AccountService;
 import com.soffid.iam.web.component.CustomField3;
 import com.soffid.iam.web.component.ObjectAttributesDiv;
+import com.soffid.iam.web.popup.CsvParser;
+import com.soffid.iam.web.popup.ImportCsvHandler;
 
+import es.caib.seycon.ng.exception.InternalErrorException;
 import es.caib.zkib.component.DataTable;
+import es.caib.zkib.component.DataTree2;
 import es.caib.zkib.component.Wizard;
+import es.caib.zkib.datamodel.DataNodeCollection;
+import es.caib.zkib.datasource.CommitException;
 import es.caib.zkib.datasource.DataSource;
 import es.caib.zkib.datasource.XPathUtils;
 import es.caib.zkib.zkiblaf.Missatgebox;
@@ -48,7 +70,7 @@ public class MenuAclHandler extends Div implements AfterCompose {
 	private CustomField3 role3;
 	private CustomField3 group3;
 	
-	public MenuAclHandler() throws NamingException, CreateException {
+	public MenuAclHandler() throws NamingException, CreateException, InternalErrorException {
 		userService = EJBLocator.getUserService();
 	}
 	
@@ -267,5 +289,139 @@ public class MenuAclHandler extends Div implements AfterCompose {
 						}
 					});
 		}
+	}
+
+	public void downloadCsv(Event event) {
+		Component lb = getListbox();
+		if (lb instanceof DataTable)
+			((DataTable) lb).download();
+		if (lb instanceof DataTree2)
+			((DataTree2) lb).download();
+	}
+
+	public void importCsv (Event ev) throws IOException, CommitException, InternalErrorException, NamingException, CreateException {
+		String[][] data = {
+				{Labels.getLabel("aplicacionsIntranet.zul.Nivell"), Labels.getLabel("aplicacionsIntranet.zul.Nivell")},
+				{Labels.getLabel("accounts.scope"), Labels.getLabel("accounts.scope")},
+				{Labels.getLabel("aplicacionsIntranet.zul.Propietari"), Labels.getLabel("aplicacionsIntranet.zul.Propietari")},
+				{Labels.getLabel("com.soffid.iam.api.Account.description"), Labels.getLabel("com.soffid.iam.api.Account.description")},
+		};
+		String title = Labels.getLabel("tenant.zul.import");
+		ImportCsvHandler.startWizard(title, data, this,
+				parser -> importCsv(parser));
+	}
+
+	private void importCsv(CsvParser parser) {
+		Map<String,String> m = null;
+		int updates = 0;
+		int inserts = 0;
+		int unchanged = 0;
+		int removed = 0;
+		String yes = CommonFns.getLabel("mesg:org.zkoss.zul.mesg.MZul:YES");
+		try {
+			List<AccessTreeAuthorization> authsInDatabase = new LinkedList<AccessTreeAuthorization>();
+			List<AccessTreeAuthorization> authsInFile = new LinkedList<AccessTreeAuthorization>();
+
+			// Retrieve current AccessTreeAuthorization from database
+			DataTable dt = (DataTable) getFellow("listbox");
+			String accessTreeName = (String) XPathUtils.eval(dt.getDataSource(), "name");
+			EntryPointService srv = EJBLocator.getEntryPointService();
+			List<AccessTree> accessTreeList = srv.findAccessTreeByTextAndJsonQuery("", "name eq \""+accessTreeName+"\"", new Integer(0), new Integer(500));
+			AccessTree accessTree = null;
+			if (accessTreeList!=null && !accessTreeList.isEmpty()) {
+				accessTree = accessTreeList.get(0);
+				Collection<AccessTreeAuthorization>	accessTreeColl = srv.getAuthorizationsApplicationAcessTree(accessTree);
+				if (!accessTreeColl.isEmpty()) {
+					Iterator<AccessTreeAuthorization> i = accessTreeColl.iterator();
+					while (i.hasNext())
+						authsInDatabase.add((AccessTreeAuthorization) i.next());
+				}
+			}
+
+			// Go through the file
+			for (Iterator<Map<String, String>> iterator = parser.iterator(); iterator.hasNext(); ) {
+				m = iterator.next();
+				String nivel = m.get(Labels.getLabel("aplicacionsIntranet.zul.Nivell"));
+				String scope = m.get(Labels.getLabel("accounts.scope"));
+				String propietario = m.get(Labels.getLabel("aplicacionsIntranet.zul.Propietari"));
+				String descripcion = m.get(Labels.getLabel("com.soffid.iam.api.Account.description"));
+				if (nivel!=null && !nivel.trim().isEmpty() &&
+						scope!=null && !scope.trim().isEmpty() &&
+						propietario!=null && !propietario.trim().isEmpty()) {
+
+					// Instanciate the file object as AccessTreeAuthorization
+					AccessTreeAuthorization ata = new AccessTreeAuthorization();
+					if (Labels.getLabel("aplicacionsIntranet.zul.Administrador").equals(nivel)) {
+						ata.setAuthorizationLevelDescription("admin");
+					} else if (Labels.getLabel("aplicacionsIntranet.zul.Autoritzat").equals(nivel)) {
+						ata.setAuthorizationLevelDescription("exec");
+					}
+					ata.setAuthorizationEntityType(scope);
+					ata.setAuthorizedEntityCode(propietario);
+					if (descripcion!=null) {
+						ata.setAuthorizedEntityDescription(descripcion);
+					} else {
+						ata.setAuthorizedEntityDescription(propietario);
+					}
+
+					// Check for changes
+					boolean foundInFile = false;
+					for (AccessTreeAuthorization ata2: authsInDatabase) {
+						if (nullCmp (ata.getAuthorizationLevelDescription(), ata2.getAuthorizationLevelDescription()) &&
+							nullCmp (ata.getAuthorizationEntityType(), ata2.getAuthorizationEntityType()) &&
+							nullCmp (ata.getAuthorizedEntityCode(), ata2.getAuthorizedEntityCode()) ) {
+							unchanged ++;
+							foundInFile = true;
+							authsInFile.add(ata);
+							break;
+						}
+					}
+					if (!foundInFile) {
+						srv.createAuthorization(accessTree, ata);
+						inserts ++;
+						authsInDatabase.add(ata);
+						authsInFile.add(ata);
+					}
+				}
+			}
+
+			// We review the ones to be deleted
+			for (AccessTreeAuthorization ata: authsInDatabase) {
+				boolean foundInFile = false;
+				for (AccessTreeAuthorization ata2: authsInFile) {
+					if (nullCmp (ata.getAuthorizationLevelDescription(), ata2.getAuthorizationLevelDescription()) &&
+						nullCmp (ata.getAuthorizationEntityType(), ata2.getAuthorizationEntityType()) &&
+						nullCmp (ata.getAuthorizedEntityCode(), ata2.getAuthorizedEntityCode()) ) {
+						foundInFile = true;
+						break;
+					}
+				}
+				if (!foundInFile) {
+					srv.deleteAuthorization(accessTree, ata);
+					removed++;
+				}
+			}
+		} catch (UiException e) {
+			throw e;
+		} catch (Exception e) {
+			if (m == null)
+				throw new UiException(e);
+			else
+				throw new UiException("Error loading parameter "+m.get("name"), e);
+		}
+
+		Missatgebox.avis(Labels.getLabel("parametres.zul.import", new Object[] { updates, inserts, removed, unchanged }));
+		DataTable dt = (DataTable) getFellow("listbox");
+		DataNodeCollection coll = (DataNodeCollection) XPathUtils.eval(dt.getDataSource(), "auth");
+		coll.commit();
+		try {
+			coll.refresh();
+		} catch (Exception e) {
+			throw new UiException(e);
+		}
+	}
+
+	private boolean nullCmp(Object object1, Object object2) {
+		return object1 == null ? object2 == null : object1.equals(object2);
 	}
 }

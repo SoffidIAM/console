410c1b69548988307181f020d7e8d7ea1e896f61
Gabriel Buades <gbuades@soffid.com>
2025-07-02 10:19:15 +0200

    Fix error in template to install a sync server via powershell

--- web/src/main/resources/com/soffid/iam/web/wheel/install-psh.sh
+++ web/src/main/resources/com/soffid/iam/web/wheel/install-psh.sh
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri syncserver.msi -OutFile  'https://download.soffid.com/maven/com/soffid/iam/sync/syncserver/VERSION/syncserver-VERSION.msi' 
+Invoke-WebRequest -OutFile syncserver.msi -Uri  'https://download.soffid.com/maven/com/soffid/iam/sync/syncserver/VERSION/syncserver-VERSION.msi' 
 msiexec /i syncserver.msi
-c:\program files\soffid\iam-sync\bin\configure -configurl 'URL'
+& 'c:\program files\soffid\iam-sync\bin\configure' -configurl 'URL'

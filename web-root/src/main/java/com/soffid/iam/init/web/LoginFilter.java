001b6c92229671071c42efd7522c3c6178170ced
Gabriel Buades <gbuades@soffid.com>
2026-03-30 11:29:37 +0200

    Fix method to jump into soffid console

--- web-root/src/main/java/com/soffid/iam/init/web/LoginFilter.java
+++ web-root/src/main/java/com/soffid/iam/init/web/LoginFilter.java
@@ -23,32 +23,31 @@ public class LoginFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 		
+		Configuration cfg = Configuration.getConfiguration();
 		if ( ! req.getRequestURI().equals("/favicon.ico") &&
 				! req.getRequestURI().startsWith("/logout") && 
-				! req.getRequestURI().startsWith("/soffid")) {
-			Configuration cfg = Configuration.getConfiguration();
-			if (cfg.isConfigured())  {
-				if ( req.getRequestURI().startsWith("/selfservice/task.zul")) {
-					resp.sendRedirect("/soffid/wf/task.zul?"+req.getQueryString());
-				}
-				else if ( req.getRequestURI().startsWith("/webservice")) {
-					String target = "/soffid"+req.getRequestURI();
-					if (req.getQueryString() != null)
-						target = target + "?"+req.getQueryString();
-					resp.setHeader("Location",  target);
-					resp.setStatus(308); // Permanent redirect
-				}
-				else if ( req.getRequestURI().startsWith("/selfservice/index.zul") &&
-						req.getParameter("target") != null &&
-						req.getParameter("target").startsWith("sharedAccounts/sharedAccounts.zul")) {
-					String qs = req.getQueryString();
-					if (qs.contains("?")) qs = qs.substring(qs.indexOf("?")+1);
-					resp.sendRedirect("/soffid/resource/account/vault.zul?"+qs);
-				}
-				else
-					resp.sendRedirect("/soffid");
-			} else
-				chain.doFilter(request, response);
+				! req.getRequestURI().startsWith("/soffid") ||
+				cfg.isConfigured())
+		{
+			if ( req.getRequestURI().startsWith("/selfservice/task.zul")) {
+				resp.sendRedirect("/soffid/wf/task.zul?"+req.getQueryString());
+			}
+			else if ( req.getRequestURI().startsWith("/webservice")) {
+				String target = "/soffid"+req.getRequestURI();
+				if (req.getQueryString() != null)
+					target = target + "?"+req.getQueryString();
+				resp.setHeader("Location",  target);
+				resp.setStatus(308); // Permanent redirect
+			}
+			else if ( req.getRequestURI().startsWith("/selfservice/index.zul") &&
+					req.getParameter("target") != null &&
+					req.getParameter("target").startsWith("sharedAccounts/sharedAccounts.zul")) {
+				String qs = req.getQueryString();
+				if (qs.contains("?")) qs = qs.substring(qs.indexOf("?")+1);
+				resp.sendRedirect("/soffid/resource/account/vault.zul?"+qs);
+			}
+			else
+				resp.sendRedirect("/soffid");
 		}
 		else
 			chain.doFilter(request, response);
